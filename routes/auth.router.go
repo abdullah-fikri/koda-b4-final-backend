7ab0559a8750678d00861fd808e2afbfdc5edc10
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-02 10:05:32 +0000

    feat: router for auth

---
+++ routes/auth.router.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func AuthRoutes(r *gin.Engine) {
+	auth := r.Group("/api/v1/auth")
+	auth.POST("/register", controllers.RegisterUser)
+	auth.POST("/login", controllers.LoginController)
+    auth.POST("/forgot-password", controllers.ForgotPassword)
+    auth.POST("/reset-password", controllers.ResetPassword)
+}
