1606e8878afb83864a7b1aab92818260fb89bd7c
abdullah-fikri <abdullahfikri981@gmail.com>
2025-12-02 10:06:10 +0000

    feat: init/main router

---
+++ routes/main.router.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Routes(r *gin.Engine){
+	// r.Use(middleware.CorsMiddleware())
+	r.GET("/", func(ctx *gin.Context){
+		ctx.JSON(200, gin.H{
+			"success": true,
+			"message": "backend is running well",
+		})
+	})
+
+	AuthRoutes(r)
+}
